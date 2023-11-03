b77a2742e8c52b7ac7ca110c81fb15e4c54cf4e8
Mike Dark <agyemankojo47@gmail.com>
2023-11-03 20:29:30 +0000

    Deploy rubocop to fix errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
 
-    # Define a method to update the comments counter for a post
-    def self.update_comments_counter(post)
-      comments_count = where(post: post).count
-      post.update(comments_counter: comments_count)
-    end
+  # Define a method to update the comments counter for a post
+  def self.update_comments_counter(post)
+    comments_count = where(post:).count
+    post.update(comments_counter: comments_count)
+  end
 end
