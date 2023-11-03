8df754ce062bbe87852a0d170bb5d35173b63783
Mike Dark <agyemankojo47@gmail.com>
2023-11-03 19:18:38 +0000

    Resolve erors with rubocop

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,7 @@
 class Like < ApplicationRecord
-    # Associations
+  # Associations
   # A like belongs to an author
   # A like belongs to a post
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
-
 end
