104196008c59c3edc5a837ed4f28c2891369ac00
Mike Dark <agyemankojo47@gmail.com>
2023-11-03 20:25:32 +0000

    Deploy rubocop to fix errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -5,9 +5,9 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
 
-    # Define a method to update the likes counter for a post
-    def self.update_likes_counter(post)
-      likes_count = where(post: post).count
-      post.update(likes_counter: likes_count)
-    end
+  # Define a method to update the likes counter for a post
+  def self.update_likes_counter(post)
+    likes_count = where(post:).count
+    post.update(likes_counter: likes_count)
+  end
 end
