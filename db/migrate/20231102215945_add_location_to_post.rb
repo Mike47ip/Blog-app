d00b09f0d67b771d811e87cf69175fd6d31b5655
Mike Dark <agyemankojo47@gmail.com>
2023-11-03 07:38:43 +0000

    Remove add and remove schema methods

--- db/migrate/20231102215945_add_location_to_post.rb
+++
@@ -1,5 +0,0 @@
-class AddLocationToPost < ActiveRecord::Migration[7.1]
-  def change
-    add_column :posts, :location, :string
-  end
-end
--- db/migrate/20231102232251_remove_location_to_post.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLocationToPost < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :posts, :location
-  end
-end
