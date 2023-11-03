b58a6a82eae41cc45356ec416743eea6b92ec85f
Mike Dark <agyemankojo47@gmail.com>
2023-11-03 20:20:34 +0000

    Create mehtods to update models

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,4 +4,10 @@ class Like < ApplicationRecord
   # A like belongs to a post
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
+
+    # Define a method to update the likes counter for a post
+    def self.update_likes_counter(post)
+      likes_count = where(post: post).count
+      post.update(likes_counter: likes_count)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,21 @@
 class User < ApplicationRecord
+  # Associations
+  # A user has many posts
+  # A user has many comments
+  # A user has many likes
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
+
+  after_save :update_posts_counter
+
+  # The 3 most recent posts for a user
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
+
+  # Updates the posts counter for a user
+  def update_posts_counter
+    update(posts_counter: posts.count)
+  end
 end
