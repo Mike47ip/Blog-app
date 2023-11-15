8745b0471c64321fbef32ac4fc3d06823c82b620
Fickry Iman <fickry.iman@gmail.com>
2023-11-15 21:26:04 +0700

    add devise method into application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-end
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:name, :bio, :email, :password)
+    end
+  end
+end
