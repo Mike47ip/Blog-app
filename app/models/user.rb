3d19aa1fc68158e999daac789214e2c83bc61864
Mike Dark <agyemankojo47@gmail.com>
2023-11-17 18:48:24 +0000

    Uncomment confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  before_validation :set_default
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+devise :database_authenticatable, :registerable,
+       :recoverable, :rememberable, :validatable,
+       :timeoutable, :trackable #:confirmable, 
+
+before_validation :set_default
+
   # :Confirmable
   # Associations
   # A user has many posts
@@ -19,4 +20,8 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def set_default
+    self.posts_counter = 0
+  end
 end
