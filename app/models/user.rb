341ccf8ac4f6c90618aa5e056993bf91b2dfb5f0
Mike Dark <agyemankojo47@gmail.com>
2023-11-18 13:16:32 +0000

    Fix sign in errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-devise :database_authenticatable, :registerable,
-       :recoverable, :rememberable, :validatable,
-       :timeoutable, :trackable #:confirmable, 
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
 
 before_validation :set_default
 
